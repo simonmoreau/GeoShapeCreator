730f1fbe323c96cd1bb03125246aefc91dccbf51
Simon Moreau <simon@bim42.com>
2016-03-21 14:17:37 +0100

    First Commit

---
+++ GeoShapeCreator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace GeoShapeCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path =  @"C:\Google Drive\05 - Travail\Code\RentFerret\CodesGéographiques\correspondance-code-insee-code-postal.csv";
+
+            List<geo>  geos = GetAreas(path);
+            List<Polygon> polygons = new List<Polygon>();
+
+            foreach (geo g in geos)
+            {
+                if (g.geo_shape.Contains("MultiPolygon"))
+                {
+                    geo_shape_multiPolygon deserializedProduct = JsonConvert.DeserializeObject<geo_shape_multiPolygon>(g.geo_shape);
+                    int polygonCount = 1;
+
+                    foreach (List<List<double>> coordinates in deserializedProduct.coordinates.FirstOrDefault())
+                    {
+                        int point_order = 1;
+                        foreach (List<double> coordinate in coordinates)
+                        {
+                            Polygon polygon = new Polygon();
+                            polygon.code_insee = g.code_insee;
+                            polygon.latitude = coordinate.FirstOrDefault();
+                            polygon.longitude = coordinate.LastOrDefault();
+                            polygon.polygon_count = polygonCount;
+                            polygon.point_order = point_order;
+
+                            polygons.Add(polygon);
+                            point_order++;
+                        }
+
+                        polygonCount++;
+                    }
+                }
+                else
+                {
+                    geo_shape_polygon deserializedProduct = JsonConvert.DeserializeObject<geo_shape_polygon>(g.geo_shape);
+
+                    int polygonCount = 1;
+
+                    int point_order = 1;
+                    foreach (List<double> coordinate in deserializedProduct.coordinates.FirstOrDefault())
+                    {
+                        Polygon polygon = new Polygon();
+                        polygon.code_insee = g.code_insee;
+                        polygon.latitude = coordinate.FirstOrDefault();
+                        polygon.longitude = coordinate.LastOrDefault();
+                        polygon.polygon_count = polygonCount;
+                        polygon.point_order = point_order;
+
+                        polygons.Add(polygon);
+                        point_order++;
+                    }
+                }
+            }
+        }
+
+        static List<geo> GetAreas(string path)
+        {
+            List<geo> geos = new List<geo>();
+
+            using (StreamReader reader = new StreamReader(path, Encoding.Default))
+            {
+                CsvReader csvReader = new CsvReader(reader);
+                csvReader.Configuration.Delimiter = ";";
+                csvReader.Configuration.WillThrowOnMissingField = false;
+                geos = csvReader.GetRecords<geo>().ToList();
+            }
+
+            return geos;
+        }
+    }
+}
---
+++ GeoShapeCreator/geo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoShapeCreator
+{
+    class geo
+    {
+        public string code_insee { get; set; }
+        public string code_postal { get; set; }
+        public string commune { get; set; }
+        public string département { get; set; }
+        public string région { get; set; }
+        public string statut { get; set; }
+        public string altitude_moyenne { get; set; }
+        public string superficie { get; set; }
+        public string population { get; set; }
+        public string geo_point_2d { get; set; }
+        public string geo_shape { get; set; }
+        public string id_geogla { get; set; }
+        public string code_commune { get; set; }
+        public string code_canton { get; set; }
+        public string code_arrondissement { get; set; }
+        public string code_département { get; set; }
+        public string code_région { get; set; }
+    }
+
+    class geo_shape_polygon
+    {
+        public string type { get; set; }
+        public List<List<List<double>>> coordinates { get; set; }
+        //public List<List<List<List<double>>>> coordinates { get; set; }
+    }
+
+    class geo_shape_multiPolygon
+    {
+        public string type { get; set; }
+        //public List<List<List<double>>> coordinates { get; set; }
+        public List<List<List<List<double>>>> coordinates { get; set; }
+    }
+
+    class Polygon
+    {
+        public string code_insee { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public int point_order { get; set; }
+        public int polygon_count { get; set; }
+    }
+}
